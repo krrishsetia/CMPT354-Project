1ac97132333e567d1750e40243b7988e77ba1845
krrish <krrishsetia3@gmail.com>
2026-04-03 15:31:42 -0700

    finished the logic for updating

--- main.py
+++ main.py
@@ -219,56 +219,195 @@ while True:
             #part is going to need to be overhauled
             selection = 'part'
             
-            #this will be a list of all types when we make a gui
             print(f"""
-                types:
-                1. Electronic
-                2. Battery
-                3. Wheel
-                4. Motor
-                5. Suspension
-                6. Structural
+                if you wish to modify the base properties of a part input the following
+                else input 6
+                Input the Base property of the part you wish to modify
+                1. Name
+                2. Weight
+                3. Height
+                4. Length
+                5. Width
+                6. SubType modifications 
                 """)
-            Type = int(input("SubType: "))
+            property = int(input("properties: "))
             
-            if Type == 1 and Type == 2:
-                MaxVoltageV = float(input("Max Voltage: "))
-                MaxCurrentA = float(input("Max Current: "))
+            print(f"""
+                Input the ID of the Sub-Assembly you wish to modify
+                """)
+            
+            Id = int(input("ID: "))
+            
+            if property == 1:
+                newName = input("New Name: ")
+                cursor.execute(f"""UPDATE part
+                            SET PartName = ?  
+                            WHERE PartID = ?""",
+                            newName,Id)
+            elif property == 2:
+                newWeight = int(input("New Weight: "))
+                cursor.execute(f"""UPDATE part
+                            SET `Weight` = ?
+                            WHERE PartID = ?""",
+                            newWeight,Id)
+            elif property == 3:
+                newHeight = input("New Height: ")
+                cursor.execute(f"""UPDATE part
+                            SET Height = ?  
+                            WHERE PartID = ?""",
+                            newHeight,Id)
+            elif property == 4:
+                newLength = int(input("New Length: "))
+                cursor.execute(f"""UPDATE part
+                            SET `Length` = ?
+                            WHERE PartID = ?""",
+                            newRobotId,Id)
+            elif property == 5:
+                newWidth = input("New Width: ")
+                cursor.execute(f"""UPDATE part
+                            SET Width = ?  
+                            WHERE PartID = ?""",
+                            newWidth,Id)
+            elif property == 6:
+            
+                # inputs id 7 times instead of doing id,id ..., id
+                parmaInput = (Id,) * 7
                 
-                cursor.execute(f"""INSERT INTO Electronic
-                        (PartID, MaxCurrentA,MaxVoltageV) VALUES  
-                        (?,?,?)""",
-                        Id,MaxCurrentA,MaxVoltageV)
-                if Type == 2:
-                    CapacitymAh = float(input("Capacity: "))
-                    
-                    cursor.execute(f"""INSERT INTO Battery
-                           (PartID, CapacitymAh) VALUES  
-                           (?,?)""",
-                           Id,CapacitymAh)
-            elif Type == 3 and Type == 4 and Type == 5:
-                cursor.execute(f"""INSERT INTO Mechanical
-                            (PartID) VALUES  (?)""", Id)
-                if Type == 3:
-                    Radius = float(input("Radius: "))
-                    WheelType = input("Wheel Type: ")
-                    
-                    cursor.execute(f"""INSERT INTO Wheel
-                            (PartID, Radius, `Type`) VALUES 
-                            (?,?,?)""", Id,Radius,WheelType)
-                elif Type == 4:
-                    Torque = float(input("Torque: "))
-                    
-                    cursor.execute(f"""INSERT INTO Motor
-                            (PartID, Torque) VALUES 
-                            (?,?)""", Id,Torque)
+                #select 'some string' will return said string if the where statment is true
+                #dual means if you find the id, print 'x' once, it for things like this
                 
-                elif Type == 5:
-                    WeightLimit = float(input("WeightLimit: "))
-                    
-                    cursor.execute(f"""INSERT INTO Suspension
-                            (PartID, WeightLimit) VALUES 
-                            (?,?)""", Id,WeightLimit)
+                cursor.execute(f""" SELECT 'Battery' FROM DUAL WHERE EXISTS (SELECT 1 FROM Battery WHERE PartID = ?)
+                                    UNION ALL
+                                    SELECT 'Electronic' FROM DUAL WHERE EXISTS (SELECT 1 FROM Electronic WHERE PartID = ?)
+                                    UNION ALL
+                                    SELECT 'Mechanical' FROM DUAL WHERE EXISTS (SELECT 1 FROM Mechanical WHERE PartID = ?)
+                                    UNION ALL
+                                    SELECT 'Structural' FROM DUAL WHERE EXISTS (SELECT 1 FROM Structural WHERE PartID = ?)
+                                    UNION ALL
+                                    SELECT 'Wheel' FROM DUAL WHERE EXISTS (SELECT 1 FROM Wheel WHERE PartID = ?)
+                                    UNION ALL
+                                    SELECT 'Motor' FROM DUAL WHERE EXISTS (SELECT 1 FROM Motor WHERE PartID = ?)
+                                    UNION ALL
+                                    SELECT 'Suspension' FROM DUAL WHERE EXISTS (SELECT 1 FROM Suspension WHERE PartID = ?);
+                            """,parmaInput) 
+                
+                rows = cursor.fetchall()
+            
+            if ('Battery',) in rows:
+                print(f"""
+                Input the property of the Battery you wish to modify
+                1. Max Current
+                2. Max Voltage
+                3. Capacity
+                """)
+                
+                subProperty = int(input("properties: "))
+                
+                if subProperty == 1:
+                    newCurrent = int(input("New Max Current: "))
+                    cursor.execute(f"""UPDATE Electronic
+                                SET MaxCurrentA = ?
+                                WHERE PartID = ?""",
+                                newCurrent,Id)
+                elif subProperty == 2:
+                    newVoltage = input("New Max Voltage: ")
+                    cursor.execute(f"""UPDATE Electronic
+                                SET MaxVoltageV = ?  
+                                WHERE PartID = ?""",
+                                newVoltage,Id)
+                elif subProperty == 3:
+                    newCapacity = int(input("New CapacitymAh: "))
+                    cursor.execute(f"""UPDATE Battery
+                                SET CapacitymAh = ?
+                                WHERE PartID = ?""",
+                                newCapacity,Id)
+            
+            elif ('Electronic',) in rows:
+                print(f"""
+                Input the property of the Electronic you wish to modify
+                1. Max Current
+                2. Max Voltage
+                """)
+                
+                subProperty = int(input("properties: "))
+                
+                if subProperty == 1:
+                    newCurrent = int(input("New Max Current: "))
+                    cursor.execute(f"""UPDATE Electronic
+                                SET MaxCurrentA = ?
+                                WHERE PartID = ?""",
+                                newCurrent,Id)
+                elif subProperty == 2:
+                    newVoltage = input("New Max Voltage: ")
+                    cursor.execute(f"""UPDATE Electronic
+                                SET MaxVoltageV = ?  
+                                WHERE PartID = ?""",
+                                newVoltage,Id)
+                
+            elif ('Structural',) in rows:
+                print(f"""
+                Input the property of the Structural part you wish to modify
+                1. Material
+                2. Type
+                """)
+                
+                subProperty = int(input("properties: "))
+                
+                if subProperty == 1:
+                    newMaterial = int(input("New Material: "))
+                    cursor.execute(f"""UPDATE Structural
+                                SET Material = ?
+                                WHERE PartID = ?""",
+                                newMaterial,Id)
+                elif subProperty == 2:
+                    newType = input("New Type: ")
+                    cursor.execute(f"""UPDATE Structural
+                                SET `Type` = ?  
+                                WHERE PartID = ?""",
+                                newType,Id)
+            
+            elif ('Wheel',) in rows:
+                print(f"""
+                Input the property of the Wheel you wish to modify
+                1. Radius
+                2. `Type`
+                """)
+                
+                subProperty = int(input("properties: "))
+                
+                if subProperty == 1:
+                    newRadius = int(input("New Radius: "))
+                    cursor.execute(f"""UPDATE Wheel
+                                SET Radius = ?
+                                WHERE PartID = ?""",
+                                newRadius,Id)
+                elif subProperty == 2:
+                    newType = input("New Type: ")
+                    cursor.execute(f"""UPDATE Wheel
+                                SET `Type` = ?  
+                                WHERE PartID = ?""",
+                                newType,Id)
+            elif ('Motor',) in rows:
+                print(f"""
+                Input the new torque for the motor
+                """)
+            
+                newTorque = input("New Torque: ")
+                cursor.execute(f"""UPDATE Motor
+                            SET Torque = ?  
+                            WHERE PartID = ?""",
+                            newType,Id)
+            elif ('Suspension',) in rows:
+                print(f"""
+                Input the new weight limit for the suspension
+                """)
+            
+                newTorque = input("New Weight Limit: ")
+                cursor.execute(f"""UPDATE Suspension
+                            SET WeightLimit = ?  
+                            WHERE PartID = ?""",
+                            newType,Id)
+                
         elif choice == 2:
             selection = "`Sub-Assembly`"
             
