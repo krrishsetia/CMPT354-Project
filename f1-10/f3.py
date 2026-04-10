801f4b0281b6be1651775bf58da97517eda92124
krrish <krrishsetia3@gmail.com>
2026-04-10 15:54:16 -0700

    made function files versions of the rest of the functions, ie i made f1-5 .py files

---
+++ f1-10/f3.py
@@ -0,0 +1,33 @@
+def f3_delete_part_subAssembly_robot(cursor):
+    # might need to be updated
+    print(f"""
+    0. to exit
+    1. delete a part
+    2. delete a sub-assembly
+    3. delete a robot
+    """)
+    selection = ""
+    choice = int(input("what do you want do to: "))
+    if choice == 0:
+        pass
+    elif choice == 1:
+        #part is going to need to be overhauled
+        selection = 'part'            
+    elif choice == 2:
+        selection = "`Sub-Assembly`"
+        
+    elif choice == 3:
+        selection = 'robot'
+    
+    Id = int(input("ID: "))
+        
+    cursor.execute(f"""DELETE FROM {selection}
+                        WHERE ID = ?)""",
+                        Id)
+    
+    # test code to see if it actually added the tuple    
+    cursor.execute(f"""SELECT * 
+                        From {selection} """)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
